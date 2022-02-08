77bcf3aabe2ba78ce84d34b774a38431904e0800
dronecuiaba <dronecuiaba@gmail.com>
2022-02-08 16:08:15 -0400

    bed level linear

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1544,8 +1544,8 @@
  *   With an LCD controller the process is guided step-by-step.
  */
 //#define AUTO_BED_LEVELING_3POINT
-//#define AUTO_BED_LEVELING_LINEAR
-#define AUTO_BED_LEVELING_BILINEAR
+#define AUTO_BED_LEVELING_LINEAR
+//#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
