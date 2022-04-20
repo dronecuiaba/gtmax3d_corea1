80040a11e489c443c26bd9adb4deb76c1ff9fbfe
dronecuiaba <dronecuiaba@gmail.com>
2022-04-20 13:07:25 -0400

    linear adv

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 404, 415 }
 
 /**
  * Default Max Feed Rate (mm/s)
